3700019c082a275f04e995adb71f4ee018c4523f
Tyla Jeffs <tylajeffs@gmail.com>
2024-01-08 17:07:32 -0500

    create random date generator

--- Selectors/script.js
+++ Selectors/script.js
@@ -1,16 +1,18 @@
-start = new Date(0, 0, 1);
-end = new Date();
-
-function randomDate(start, end) {
-  date = new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-  document.getElementById("date").innerHTML = date.toString();
-}
-
-function generateRandomDate() {
-  month = Math.floor(Math.random() * 13);
-  day = Math.floor(Math.random() * 355);
-  year = Math.floor(Math.random() * 2025);
-  document.getElementById("date").innerHTML = month + " " + day + " " + year;
+function randomDate(date1, date2) {
+  function randomValueBetween(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  var date1 = date1 || "01-01-1970";
+  var date2 = date2 || new Date().toLocaleDateString();
+  date1 = new Date(date1).getTime();
+  date2 = new Date(date2).getTime();
+  if (date1 > date2) {
+    document.getElementById("date").innerHTML = new Date(
+      randomValueBetween(date2, date1)
+    ).toLocaleDateString();
+  } else {
+    document.getElementById("date").innerHTML = new Date(
+      randomValueBetween(date1, date2)
+    ).toLocaleDateString();
+  }
 }
